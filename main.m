79309dfcd0db447a37e24fe9289d36078af191e3
itayzach <zitay.zach@gmail.com>
2019-12-14 14:25:15 +0200

    refactored to have multiple SNRs in nice figures

--- main.m
+++ main.m
@@ -7,85 +7,99 @@ f0 = 1e3;             % singletone [Hz]
 w0 = 2*pi*f0;         % [rad/sec]
 fs = 8e3;             % sampling rate [Hz]
 B = 1;                % amplitude [V]
-delta = 100e-3;        % uniform spacing between sensors [m]
+delta = 0.1;          % uniform spacing between sensors [m]
 c = 340;              % speed of sound [m/s]
 
-theta = 70.3;                    % [degrees]
-tau = delta*cos(theta*pi/180)/c; % [sec]
-
-d = exp(-1j*w0*(m-1)*tau); % steering vector
+theta = 70.3;                       % [degrees]
+tau = delta*cos(theta*pi/180)*fs/c; % [samples]
+d = exp(-1j*w0/fs*(m-1)*tau);       % steering vector
 
 %% Signals
 % after a bandpass filter around f0 (filter out the negative spectrum)
-x1_bp_noiseless = B*exp(1j*w0*n);
-SNR = inf;
-sigma = B/10^(SNR/20);
-awgn = sigma*randn(N,1);
-x1_bp = x1_bp_noiseless + awgn;
-disp(snr(x1_bp_noiseless, awgn))
-
-% build time-space signal matrix
-x_bp_M = repmat(x1_bp, 1, M);
-d_N = repmat(d, N, 1);
-x_nm = x_bp_M.*d_N;
+x1_bp_noiseless = B*exp(1j*w0/fs*n);
+SNR_vec = [inf 5];
 
 est_theta_vec = 0:180;
-% est_theta_vec = [30 70 120];
-piquancy = zeros(size(est_theta_vec));
+% est_theta_vec = [theta 30 120];
+piquancy = zeros(length(SNR_vec), length(est_theta_vec));
+    
+for SNR_idx = 1:length(SNR_vec)
+    SNR = SNR_vec(SNR_idx);
+    sigma = B/10^(SNR/20);
+    awgn = sigma*randn(N,1);
+    x1_bp = x1_bp_noiseless + awgn;
+    disp(snr(x1_bp_noiseless, awgn))
 
-for th_idx = 1:length(est_theta_vec)
-    %% Space-domain adjacency matrix
-    est_theta = est_theta_vec(th_idx);
-    est_tau = delta*cos(est_theta*pi/180)/c;    % [sec]
+    % build time-space signal matrix
+    x_bp_M = repmat(x1_bp, 1, M);
+    d_N = repmat(d, N, 1);
+    x_nm = x_bp_M.*d_N;
 
-    a1_r = 0.5*[0 exp(1j*w0*est_tau) zeros(1,M-3) exp(1j*w0*(M-1)*est_tau)];
-    a1_c = a1_r'; % transpose and conj
-    A1 = toeplitz(a1_c, a1_r);
+    for th_idx = 1:length(est_theta_vec)
+        %% Space-domain adjacency matrix
+        est_theta = est_theta_vec(th_idx);
+        est_tau = delta*cos(est_theta*pi/180)*fs/c; % [samples]
 
-    % verify eigen vector with eigenvalue = 1
-    % for k = 1:N
-    %     x_k = x_nm(k,:).';
-    %     assert(norm(1*x_k - A1*x_k) < 1e-10);
-    % end
+        a1_r = 0.5*[0 exp(1j*w0/fs*est_tau) zeros(1,M-3) exp(1j*w0/fs*(M-1)*est_tau)];
+        a1_c = a1_r'; % transpose and conj
+        A1 = toeplitz(a1_c, a1_r);
 
-    %% Time-domain adjacency matrix
-    a2_r = 0.5*[0 exp(-1j*w0) zeros(1,N-3) exp(-1j*w0*(N-1))];
-    a2_c = a2_r'; % transpose and conj
-    A2 = toeplitz(a2_c, a2_r);
+        % verify eigen vector with eigenvalue = 1
+        if (est_theta == theta)
+            for k = 1:N
+                x_k = x_nm(k,:).';
+                assert(norm(1*x_k - A1*x_k) < 1e-10);
+            end
+        end
 
-    % verify eigen vector with eigenvalue = 1
-%     for k = 1:M
-%         x_k = x_nm(:,k);
-%         assert(norm(1*x_k - A2*x_k) < 1e-10);
-%     end
+        %% Time-domain adjacency matrix
+        a2_r = 0.5*[0 exp(-1j*w0/fs) zeros(1,N-3) exp(-1j*w0/fs*(N-1))];
+        a2_c = a2_r'; % transpose and conj
+        A2 = toeplitz(a2_c, a2_r);
 
-    %% Space-Time graph
-    A = kron(A2, A1);
-    x_mn = x_nm.';
-    x = x_mn(:);
+        % verify eigen vector with eigenvalue = 1
+        if (est_theta == theta)
+            for k = 1:M
+                x_k = x_nm(:,k);
+                assert(norm(1*x_k - A2*x_k) < 1e-10);
+            end
+        end
 
-    % assert(norm(1*x - A*x) < 1e-9);
+        %% Space-Time graph
+        A = kron(A2, A1);
+        x_mn = x_nm.';
+        x = x_mn(:);
+        if (est_theta == theta)
+            assert(norm(1*x - A*x) < 1e-9);
+        end
 
-    %% GSP-toolbox
-    % Gkron_coords = [(1:M*N)', zeros(M*N,1)];
-    % Gkron_plotting_limits = [ ];
-    % 
-    % G_kron = gsp_graph(A); %, Gkron_coords, Gkron_plotting_limits);
-    % G_kron = gsp_compute_fourier_basis(G_kron);
+        %% GSP-toolbox
+        % Gkron_coords = [(1:M*N)', zeros(M*N,1)];
+        % Gkron_plotting_limits = [ ];
+        % 
+        % G_kron = gsp_graph(A); %, Gkron_coords, Gkron_plotting_limits);
+        % G_kron = gsp_compute_fourier_basis(G_kron);
 
-    % x_hat = G_kron.U' * x;
+        % x_hat = G_kron.U' * x;
 
 
-    %% GFT
-    [V, Lambda] = eig(A);
-    lambda = diag(Lambda);
-    i_eig = find(abs(lambda - 1) < 1e-10);
-    assert(length(i_eig) == 1, 'found more than one i_eig');
-    x_hat = V' * x;
-%     figure; stem(abs(x_hat))
-    x_hat_normed = x_hat/abs(x_hat(i_eig));
-    x_hat_ = x_hat_normed([1:i_eig-1 i_eig+1:end]);
-    piquancy(th_idx) = 1/sqrt(sum(abs(x_hat_).^2));
+        %% GFT
+        [V, Lambda] = eig(A);
+        lambda = diag(Lambda);
+        i_eig = find(abs(lambda - 1) < 1e-10);
+        assert(length(i_eig) == 1, 'found more than one i_eig');
+        x_hat = V' * x;
+    %     figure; stem(abs(x_hat))
+        x_hat_normed = x_hat/abs(x_hat(i_eig));
+        x_hat_ = x_hat_normed([1:i_eig-1 i_eig+1:end]);
+        piquancy(SNR_idx, th_idx) = 1/sqrt(sum(abs(x_hat_).^2));
+    end
+    piquancy(SNR_idx,:) = piquancy(SNR_idx,:)/max(piquancy(SNR_idx,:));
 end
-piquancy = piquancy/max(piquancy);
-figure; plot(est_theta_vec, piquancy);
+figure; 
+for SNR_idx = 1:length(SNR_vec)
+    plot(est_theta_vec, piquancy(SNR_idx,:), 'LineWidth', 2); hold on;
+end
+xline(theta, '--r');
+xlabel('$\theta$', 'Interpreter', 'latex', 'FontSize', 14);
+ylabel('$\xi(\theta)$', 'Interpreter', 'latex', 'FontSize', 14);
