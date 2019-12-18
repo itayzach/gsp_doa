d533c78747dd827af46c2aaa9ed934a9340efdac
itayzach <zitay.zach@gmail.com>
2019-12-18 10:39:09 +0200

    attempt of using ComplexNumber and regular PyTorch

--- gcn_for_doa.py
+++ gcn_for_doa.py
@@ -10,16 +10,79 @@ import torch.nn.functional as F
 import torch.optim
 from sklearn.metrics import classification_report
 import scipy
+from pytorch_complex_tensor import ComplexTensor
+torch.set_printoptions(threshold=10000)
 
-ID_INSTR = 0
-ID_ADMIN = 33
+# Parmeters
+N = 41
+n = np.arange(0, N)    # samples
+M = 6
+m = np.arange(1, M+1)  # sensors
+f0 = 1e3               # singletone[Hz]
+w0 = 2 * np.pi * f0    # [rad / sec]
+fs = 8e3               # sampling rate[Hz]
+B = 1                  # amplitude[V]
+delta = 0.1            # uniform spacing between sensors[m]
+c = 340                # speed of sound[m / s]
 
+K = 100
+theta_d = 70.3  # [degrees]
 
 def signaltonoise(a, axis=0, ddof=0):
     a = np.asanyarray(a)
     m = a.mean(axis)
     sd = a.std(axis=axis, ddof=ddof)
     return np.where(sd == 0, 0, m / sd)
+
+
+def getNoiselessSignal(theta):
+    tau = delta * np.cos(theta * np.pi / 180) * fs / c  # [samples]
+    d = np.exp(-1j * w0 / fs * (m - 1) * tau)  # steering vector
+
+    # % % Signals
+    # % after a bandpass filter around f0 (filter out the negative spectrum)
+    x1_bp = B * np.exp(1j * w0 / fs * n)
+    x_bp_M = np.matlib.repmat(np.transpose(np.column_stack(x1_bp)), 1, M)
+    d_N = np.matlib.repmat(d, N, 1)
+    x_nm = np.multiply(x_bp_M, d_N)
+    x_noiseless = np.ravel(x_nm)
+
+    return x_noiseless
+
+
+def generateTrainingData():
+    SNR_vec = np.linspace(start=-20, stop=20, num=int(K / 2))
+
+    X_true = np.zeros((N * M, int(K / 2)), dtype=complex)
+    # Generate training data of True
+    for SNR_idx, SNR in enumerate(SNR_vec):
+        # Signal
+        sigma = B / 10 ** (SNR / 20)
+        mu = 0
+        awgn = sigma * np.random.normal(mu, sigma, N * M)
+
+        x_noiseless = getNoiselessSignal(theta=70.3)
+        X_true[:, SNR_idx] = x_noiseless + awgn
+
+    X_false = np.zeros((N * M, int(K / 2)), dtype=complex)
+    for SNR_idx, SNR in enumerate(SNR_vec):
+        theta = np.random.uniform(0, 180)  # [degrees]
+        x_noisless = getNoiselessSignal(theta)
+
+        # Signal
+        sigma = B / 10 ** (SNR / 20)
+        mu = 0
+        awgn = sigma * np.random.normal(mu, sigma, N * M)
+
+        X_false[:, SNR_idx] = x_noisless + awgn
+
+    labels = np.concatenate((np.full(K, True, dtype=int), np.full(K, False, dtype=int)))
+    X = np.concatenate((X_true, X_false), axis=1)
+
+    labels = torch.tensor(labels, dtype=torch.long)
+    X = ComplexTensor(X)
+
+    return X, labels
 
 
 class GCNLayer(nn.Module):
@@ -43,8 +106,11 @@ class GCNLayer(nn.Module):
     def forward(self, X):
         Z = torch.tensor(0.)
         for k, fc in enumerate(self.fc_layers):
-            L = self.laplacians[k]
-            LX = torch.mm(L, X)
+            L_real = self.laplacians[k]
+            L_imag = torch.zeros(L_real.shape)
+            L = ComplexTensor(torch.cat((L_real, L_imag)))
+            # L = self.laplacians[k]
+            LX = L.mm(X)
             Z = fc(LX) + Z
 
         return torch.relu(Z)
@@ -57,7 +123,7 @@ def train_node_classifier(model, optimizer, X, y, epochs=60, print_every=10):
         y_pred_epochs.append(y_pred.detach())
 
         # Semi-supervised: only use labels of the Instructor and Admin nodes
-        labelled_idx = [ID_ADMIN, ID_INSTR]
+        labelled_idx = [0, 1]
         loss = F.nll_loss(y_pred[labelled_idx], y[labelled_idx])
 
         optimizer.zero_grad()
@@ -74,137 +140,129 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'running on: {device}')
 
-    # % % Parmeters
-    N = 41
-    n = np.arange(0, N)    # samples
-    M = 6
-    m = np.arange(1, M+1)  # sensors
-    f0 = 1e3               # singletone[Hz]
-    w0 = 2 * np.pi * f0    # [rad / sec]
-    fs = 8e3               # sampling rate[Hz]
-    B = 1                  # amplitude[V]
-    delta = 0.1            # uniform spacing between sensors[m]
-    c = 340                # speed of sound[m / s]
-
-    theta = 70.3  # [degrees]
-    tau = delta * np.cos(theta * np.pi / 180) * fs / c  # [samples]
-    d = np.exp(-1j * w0 / fs * (m - 1) * tau)  # steering vector
-
-    # % % Signals
-    # % after a bandpass filter around f0 (filter out the negative spectrum)
-    x1_bp = B * np.exp(1j * w0 / fs * n)
-    x_bp_M = np.matlib.repmat(np.transpose(np.column_stack(x1_bp)), 1, M)
-    d_N = np.matlib.repmat(d, N, 1)
-    x_nm = np.multiply(x_bp_M, d_N)
-    x_noisless = np.ravel(x_nm)
-    est_theta_vec = np.arange(0, 180)
-    piquancy = np.zeros(len(est_theta_vec))
-
-    SNR_vec = np.array([5, float("inf")])  # [dB]
-
-    for SNR_idx, SNR in enumerate(SNR_vec):
-        # Signal
-        sigma = B / 10 ** (SNR / 20)
-        mu = 0
-        awgn = sigma * np.random.normal(mu, sigma, N * M)
-
-        x = x_noisless + awgn
-        print('SNR = {0:.2f}'.format(signaltonoise(abs(x))))
-        for th_idx, est_theta in enumerate(est_theta_vec):
-
-            # Adjacency matrix
-            est_tau = delta * np.cos(est_theta * np.pi / 180) * fs / c  # [samples]
-
-            a1_r = 0.5 * np.concatenate((np.array([0, np.exp(1j * w0 / fs * est_tau)]),
-                                         np.zeros(M - 3),
-                                         np.array([np.exp(1j * w0 / fs * (M - 1) * est_tau)])),
-                                        axis=0)
-            a1_c = a1_r.conj().T  # transpose and conj
-            A1 = scipy.linalg.toeplitz(a1_c, a1_r)
-
-            a2_r = 0.5 * np.concatenate((np.array([0, np.exp(-1j * w0 / fs)]),
-                                         np.zeros(N - 3),
-                                         np.array([np.exp(-1j * w0 / fs * (N - 1))])),
-                                        axis=0)
-            a2_c = a2_r.conj().T  # transpose and conj
-            A2 = scipy.linalg.toeplitz(a2_c, a2_r)
-
-            A = np.kron(A2, A1)
-
-            if est_theta == theta:
-                assert (np.linalg.norm(1 * x_noisless - np.matmul(A, x_noisless)) < 1e-9)
-
-            G = nx.from_numpy_matrix(A)
-            #G = nx.karate_club_graph()
-            #ID_MEMBERS = set(G.nodes()) - {ID_ADMIN, ID_INSTR}
-
-            # Visualize the Karate Club graph
-            # fig, ax = plt.subplots(1,1, figsize=(14,8), dpi=100)
-            # pos = nx.spring_layout(G)
-            # cmap = cmap=plt.cm.tab10
-            # node_colors = 0.4*np.ones(G.number_of_nodes())
-            # node_colors[ID_INSTR] = 0.
-            # node_colors[ID_ADMIN] = 1.
-            # node_labels = {i: i for i in ID_MEMBERS}
-            # node_labels.update({i: l for i,l in zip([ID_ADMIN, ID_INSTR],['A','I'])})
-            # nx.draw_networkx(G, pos, node_color=node_colors, labels=node_labels, ax=ax, cmap=cmap)
-
-            # Adjacency
-            # A = nx.adj_matrix(G, weight=None)
-            # A = np.array(A.todense())
-            # Degree matrix
-            dii = np.sum(A, axis=1, keepdims=False)
-            D = np.diag(dii)
-            # Laplacian
-            L = D - A
-            w, Phi = np.linalg.eigh(L)
-
-            # Plot spectrum
-            # plt.plot(w); plt.xlabel(r'$\lambda$')
-
-            # Plot Fourier basis
-            # fig, ax = plt.subplots(4, 4, figsize=(8,6), dpi=150)
-            # ax = ax.reshape(-1)
-            # vmin, vmax = np.min(Phi), np.max(Phi)
-            # for i in range(len(ax)):
-            #     nc = Phi[:,i]
-            #     nx.draw_networkx(G, pos, node_color=nc, with_labels=False, node_size=15, ax=ax[i], width=0.4, cmap=plt.cm.magma, vmin=vmin, vmax=vmax)
-            #     ax[i].axis('off')
-            #     ax[i].set_title(rf'$\lambda_{{{i}}}={w[i]:.2f}$',fontdict=dict(fontsize=8))
-
-            llambda, V = np.linalg.eigh(A)
-            i_eig = np.argwhere(abs(llambda - 1) < 1e-10).ravel()
-            i_eig = i_eig[0]
 
 
-            x_hat = np.matmul(V.conj().T, x)
-            # % figure; stem(abs(x_hat))
-            x_hat_normed = x_hat / abs(x_hat[i_eig])
-            x_hat_ = np.delete(x_hat_normed, i_eig)
-            piquancy[th_idx] = 1 / np.sqrt(sum(abs(x_hat_) ** 2))
-        piquancy = piquancy / max(piquancy)
-        plt.axvline(x=theta, color='r')
-        plt.plot(piquancy, label='SNR = ' + str(SNR) + ' [dB]', linewidth=2)
-        plt.xlabel(r'$\theta$')
-        plt.ylabel(r'$\xi(\theta)$')
-    plt.legend(loc='upper right')
-    plt.xticks(np.arange(0, 180, step=20))
-    plt.xlim(est_theta_vec[0], est_theta_vec[-1])
-    plt.ylim(0, 1)
-    plt.show()
+    # est_theta_vec = np.arange(0, 180)
+    # piquancy = np.zeros(len(est_theta_vec))
 
-    assert(True, 'Continue from here...')
-    # Input: features will be one-hot vectors (no actual info conveyed)
-    X = torch.eye(G.number_of_nodes())
+    # SNR_vec = np.array([5, float("inf")])  # [dB]
+    # for SNR_idx, SNR in enumerate(SNR_vec):
+    #     # Signal
+    #     sigma = B / 10 ** (SNR / 20)
+    #     mu = 0
+    #     awgn = sigma * np.random.normal(mu, sigma, N * M)
+    #
+    #     x_noiseless = getNoiselessSignal(theta)
+    #     x = x_noiseless + awgn
+    #     print('SNR = {0:.2f}'.format(signaltonoise(abs(x))))
+    #     for th_idx, est_theta in enumerate(est_theta_vec):
+    #
+    #         # Adjacency matrix
+    #         est_tau = delta * np.cos(est_theta * np.pi / 180) * fs / c  # [samples]
+    #
+    #         a1_r = 0.5 * np.concatenate((np.array([0, np.exp(1j * w0 / fs * est_tau)]),
+    #                                      np.zeros(M - 3),
+    #                                      np.array([np.exp(1j * w0 / fs * (M - 1) * est_tau)])),
+    #                                     axis=0)
+    #         a1_c = a1_r.conj().T  # transpose and conj
+    #         A1 = scipy.linalg.toeplitz(a1_c, a1_r)
+    #
+    #         a2_r = 0.5 * np.concatenate((np.array([0, np.exp(-1j * w0 / fs)]),
+    #                                      np.zeros(N - 3),
+    #                                      np.array([np.exp(-1j * w0 / fs * (N - 1))])),
+    #                                     axis=0)
+    #         a2_c = a2_r.conj().T  # transpose and conj
+    #         A2 = scipy.linalg.toeplitz(a2_c, a2_r)
+    #
+    #         A = np.kron(A2, A1)
+    #
+    #         if est_theta == theta:
+    #             assert (np.linalg.norm(1 * x_noiseless - np.matmul(A, x_noiseless)) < 1e-9)
+    #
+    #         G = nx.from_numpy_matrix(A)
+    #         #G = nx.karate_club_graph()
+    #         #ID_MEMBERS = set(G.nodes()) - {ID_ADMIN, ID_INSTR}
+    #
+    #         # Visualize the Karate Club graph
+    #         # fig, ax = plt.subplots(1,1, figsize=(14,8), dpi=100)
+    #         # pos = nx.spring_layout(G)
+    #         # cmap = cmap=plt.cm.tab10
+    #         # node_colors = 0.4*np.ones(G.number_of_nodes())
+    #         # node_colors[ID_INSTR] = 0.
+    #         # node_colors[ID_ADMIN] = 1.
+    #         # node_labels = {i: i for i in ID_MEMBERS}
+    #         # node_labels.update({i: l for i,l in zip([ID_ADMIN, ID_INSTR],['A','I'])})
+    #         # nx.draw_networkx(G, pos, node_color=node_colors, labels=node_labels, ax=ax, cmap=cmap)
+    #
+    #         # Adjacency
+    #         # A = nx.adj_matrix(G, weight=None)
+    #         # A = np.array(A.todense())
+    #         # Degree matrix
+    #         dii = np.sum(A, axis=1, keepdims=False)
+    #         D = np.diag(dii)
+    #         # Laplacian
+    #         L = D - A
+    #         w, Phi = np.linalg.eigh(L)
+    #
+    #         # Plot spectrum
+    #         # plt.plot(w); plt.xlabel(r'$\lambda$')
+    #
+    #         # Plot Fourier basis
+    #         # fig, ax = plt.subplots(4, 4, figsize=(8,6), dpi=150)
+    #         # ax = ax.reshape(-1)
+    #         # vmin, vmax = np.min(Phi), np.max(Phi)
+    #         # for i in range(len(ax)):
+    #         #     nc = Phi[:,i]
+    #         #     nx.draw_networkx(G, pos, node_color=nc, with_labels=False, node_size=15, ax=ax[i], width=0.4, cmap=plt.cm.magma, vmin=vmin, vmax=vmax)
+    #         #     ax[i].axis('off')
+    #         #     ax[i].set_title(rf'$\lambda_{{{i}}}={w[i]:.2f}$',fontdict=dict(fontsize=8))
+    #
+    #         llambda, V = np.linalg.eigh(A)
+    #         i_eig = np.argwhere(abs(llambda - 1) < 1e-10).ravel()
+    #         i_eig = i_eig[0]
+    #
+    #
+    #         x_hat = np.matmul(V.conj().T, x)
+    #         # % figure; stem(abs(x_hat))
+    #         x_hat_normed = x_hat / abs(x_hat[i_eig])
+    #         x_hat_ = np.delete(x_hat_normed, i_eig)
+    #         piquancy[th_idx] = 1 / np.sqrt(sum(abs(x_hat_) ** 2))
+    #     piquancy = piquancy / max(piquancy)
+    #     plt.axvline(x=theta, color='r')
+    #     plt.plot(piquancy, label='SNR = ' + str(SNR) + ' [dB]', linewidth=2)
+    #     plt.xlabel(r'$\theta$')
+    #     plt.ylabel(r'$\xi(\theta)$')
+    # plt.legend(loc='upper right')
+    # plt.xticks(np.arange(0, 180, step=20))
+    # plt.xlim(est_theta_vec[0], est_theta_vec[-1])
+    # plt.ylim(0, 1)
+    # plt.show()
 
-    # Create ground-truth labels
-    labels = [(0 if d['club']=='Mr. Hi' else 1) for i,d in G.nodes().data()]
-    labels = torch.tensor(labels, dtype=torch.long)
-
-    # Labels represent group affiliation
-    list(G.nodes().data())
+    # Create ground-truth data
+    X, labels = generateTrainingData()
 
     # Adjacency matrix
+    # Space-domain adjacency
+    tau_d = delta * np.cos(theta_d * np.pi / 180) * fs / c  # [samples]
+    a1_r = 0.5 * np.concatenate((np.array([0, np.exp(1j * w0 / fs * tau_d)]),
+                                 np.zeros(M - 3),
+                                 np.array([np.exp(1j * w0 / fs * (M - 1) * tau_d)])),
+                                axis=0)
+    a1_c = a1_r.conj().T  # transpose and conj
+    A1 = scipy.linalg.toeplitz(a1_c, a1_r)
+
+    # Time-domain adjacency
+    a2_r = 0.5 * np.concatenate((np.array([0, np.exp(-1j * w0 / fs)]),
+                                 np.zeros(N - 3),
+                                 np.array([np.exp(-1j * w0 / fs * (N - 1))])),
+                                axis=0)
+    a2_c = a2_r.conj().T  # transpose and conj
+    A2 = scipy.linalg.toeplitz(a2_c, a2_r)
+
+    # Space-Time adjacency
+    A = np.kron(A2, A1)
+
+    G = nx.from_numpy_matrix(A)
     A = nx.adj_matrix(G, weight=None)
     A = np.array(A.todense())
     I = np.eye(A.shape[0])
