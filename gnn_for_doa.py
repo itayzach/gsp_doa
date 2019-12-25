2b6b47ae9e401f95fa2a2c8a20e8da75ef520586
itayzach <zitay.zach@gmail.com>
2019-12-25 18:25:49 +0200

    big change - compare both methods as function of SNR

--- gnn_for_doa.py
+++ gnn_for_doa.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
-from gsp import laplacian_doa, plot_random_signal
-from gcn import GraphNet, GraphSignalsDataset, plot_accuracy
+from gsp import gsp_doa, plot_random_signal
+from gnn import GraphNet, GraphSignalsDataset, plot_accuracy, gnn_doa
 from parameters import plots_dir
 from utils import mkdir_p
 
@@ -26,7 +26,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
         pred = output.argmax(dim=1, keepdim=True)
         correct += pred.eq(target.view_as(pred)).sum().item()
-    return correct/len(train_loader.dataset)
+    return 100.0*correct/len(train_loader.dataset)
 
 
 def test(args, model, device, test_loader):
@@ -43,8 +43,21 @@ def test(args, model, device, test_loader):
 
     test_loss /= len(test_loader.dataset)
 
-    print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset):.0f}%)\n')
-    return correct/len(test_loader.dataset)
+    print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset):.2f}%)\n')
+    return 100.0*correct/len(test_loader.dataset)
+
+
+def compare_methods(snr_vec, gsp_doa_accuracy_vs_snr, gnn_doa_accuracy_vs_snr):
+    fig = plt.figure()
+    plt.plot(snr_vec, gsp_doa_accuracy_vs_snr, label='GSP', linewidth=2)
+    plt.plot(snr_vec, gnn_doa_accuracy_vs_snr, label='GNN', linewidth=2)
+    plt.legend()
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('Accuracy [%]')
+    plt.xlim(snr_vec[0], snr_vec[-1])
+    plt.ylim(0, 100)
+    fig.savefig(plots_dir + '/compare_methods_accuracy.png', dpi=200)
+    plt.show()
 
 
 def main():
@@ -57,17 +70,20 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=5,
                         help='input batch size for testing (default: 5)')
-    parser.add_argument('--epochs', type=int, default=100,
+    parser.add_argument('--epochs', type=int, default=2,
                         help='number of epochs to train (default: 100)')
-    parser.add_argument('--lr', type=float, default=0.001,
+    parser.add_argument('--lr', type=float, default=0.003,
                         help='learning rate (default: 0.001)')
     parser.add_argument('--seed', type=int, default=1,
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=100,
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--plot_gsp_figs', type=int, default=True,
+    parser.add_argument('--plot-gsp-figs', type=bool, default=False,
                         help='plot GSP figures')
-
+    parser.add_argument('--run-gsp-doa', type=bool, default=False,
+                        help='Run Graph Signal Processing DOA estimation')
+    parser.add_argument('--run-gnn-doa', type=bool, default=True,
+                        help='Run Graph Neural Network DOA estimation')
     args = parser.parse_args()
     use_cuda = False
 
@@ -76,16 +92,23 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_set = GraphSignalsDataset(K=5000)
+    snr_vec = np.arange(start=-10, stop=14, step=2)
+    train_set = GraphSignalsDataset(num_true_points_per_snr=1000,
+                                    num_false_points_per_snr=1000,
+                                    snr_vec=snr_vec)
     train_loader = torch.utils.data.DataLoader(
         train_set,
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    test_set = GraphSignalsDataset(K=1500)
+    test_set = GraphSignalsDataset(num_true_points_per_snr=250,
+                                   num_false_points_per_snr=250,
+                                   snr_vec=snr_vec)
     test_loader = torch.utils.data.DataLoader(
         test_set,
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    print(f'Training set size = {train_set.__len__()}')
+    print(f'Test set size = {test_set.__len__()}')
     ####################################################################################################################
     # prev network
     ####################################################################################################################
@@ -94,41 +117,62 @@ def main():
     # max_deg = 2
     # hidden_dim = in_features
     #
-    # # Stack two GCN layers as our model
-    # # gcn2 = nn.Sequential(
-    # #     GCNLayer(L, in_features, hidden_dim, max_deg),
-    # #     GCNLayer(L, hidden_dim, out_features, max_deg),
+    # # Stack two gnn layers as our model
+    # # gnn2 = nn.Sequential(
+    # #     gnnLayer(L, in_features, hidden_dim, max_deg),
+    # #     gnnLayer(L, hidden_dim, out_features, max_deg),
     # #     ComplexToAbs(dim=1),
     # #     nn.LogSoftmax(dim=1)
     # # )
-    # gcn2 = GNN(L, in_features, hidden_dim, out_features, max_deg).to(device)
-    # print(gcn2)
+    # gnn2 = GNN(L, in_features, hidden_dim, out_features, max_deg).to(device)
+    # print(gnn2)
 
-    model = GraphNet().to(device)
-    print(model)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=1e-1)
-    print('number of trainable parameters: %d' %
-          np.sum([np.prod(p.size()) if p.requires_grad else 0 for p in model.parameters()]))
+    ####################################################################################################################
+    # Graph Signal Processing DOA
+    ####################################################################################################################
+    if args.run_gsp_doa:
+        gsp_doa_est_theta_vec, gsp_doa_est_labels_vec, gsp_doa_accuracy_vs_snr = gsp_doa(test_set)
+        print("GSP accuracy:")
+        np.set_printoptions(precision=2)
+        print(gsp_doa_accuracy_vs_snr)
 
-    train_acc_vec = np.array([])
-    test_acc_vec = np.array([])
+    ####################################################################################################################
+    # Graph Neural Network DOA
+    ####################################################################################################################
+    if args.run_gnn_doa:
+        model = GraphNet().to(device)
+        print(model)
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=1e-1)
+        print('number of trainable parameters: %d' %
+              np.sum([np.prod(p.size()) if p.requires_grad else 0 for p in model.parameters()]))
 
-    if args.plot_gsp_figs:
-        laplacian_doa()
-        plot_random_signal(train_set.get_signals(), label=True, snr=0)
-        plot_random_signal(train_set.get_signals(), label=False, snr=0)
-        plot_random_signal(train_set.get_signals(), label=True, snr=5)
-        plot_random_signal(train_set.get_signals(), label=False, snr=5)
+        train_acc_vec = np.array([])
+        test_acc_vec = np.array([])
 
-    for epoch in range(1, args.epochs + 1):
-        train_acc = train(args, model, device, train_loader, optimizer, epoch)
-        test_acc = test(args, model, device, test_loader)
+        if args.plot_gsp_figs:
+            plot_random_signal(train_set.get_signals(), label=True, snr=0)
+            plot_random_signal(train_set.get_signals(), label=False, snr=0)
+            plot_random_signal(train_set.get_signals(), label=True, snr=5)
+            plot_random_signal(train_set.get_signals(), label=False, snr=5)
 
-        train_acc_vec = np.append(train_acc_vec, 100.0*train_acc)
-        test_acc_vec = np.append(test_acc_vec, 100.0*test_acc)
+        for epoch in range(1, args.epochs + 1):
+            train_acc = train(args, model, device, train_loader, optimizer, epoch)
+            test_acc = test(args, model, device, test_loader)
 
-    plot_accuracy(args, train_acc_vec, test_acc_vec)
-    plt.show()
+            train_acc_vec = np.append(train_acc_vec, train_acc)
+            test_acc_vec = np.append(test_acc_vec, test_acc)
+
+        plot_accuracy(args, train_acc_vec, test_acc_vec)
+
+        gnn_doa_est_theta_vec, gnn_doa_est_labels_vec, gnn_doa_accuracy_vs_snr = gnn_doa(model, test_set)
+        print("GNN accuracy:")
+        np.set_printoptions(precision=2)
+        print(gnn_doa_accuracy_vs_snr)
+
+    if args.run_gsp_doa and args.run_gnn_doa:
+        compare_methods(test_set.signals['snr'], gsp_doa_accuracy_vs_snr, gnn_doa_accuracy_vs_snr)
+
+    print("All done.")
 
 
 if __name__ == '__main__':
